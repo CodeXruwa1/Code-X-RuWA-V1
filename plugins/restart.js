3ec8aa830085688077f49ff578946d5c25fb3701
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-18 18:11:43 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -12,7 +12,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
     if(!isOwner) return
 const {exec} = require("child_process")
-reply("restarting...")
+reply("Code-X-RuWA-V1 Bot Is Restarting...")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
