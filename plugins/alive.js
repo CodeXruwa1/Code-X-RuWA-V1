26113a47ac0cb00d2ff5a5ecc9af4becb67a9b4a
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-19 22:51:53 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,9 +10,9 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let aliveImg = "https://github.com/CodeXRuWA/Code-X-RuWA-V1-Alive-img/blob/main/Code-X-RuWA-V1%20Alive%20Logo.jpg?raw=true",
+let  aliveImg = "https://github.com/CodeXRuWA/Code-X-RuWA-V1-Alive-img/blob/main/Code-X-RuWA-V1%20Alive%20Logo.jpg?raw=true",
 
-let aliveMsg = "Hello' i am code-X-RuWA-V1 I am Alive",
+let  aliveMsg = "Hello' i am code-X-RuWA-V1 I am Alive",
     
 return await conn.sendMessage(from,{image: {url: aliveImg },caption: aliveMsg },{quoted: mek})
 }catch(e){
