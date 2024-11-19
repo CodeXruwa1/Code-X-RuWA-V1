8eac14d4f20daabf7b89214e05cab3d74ab8c5c5
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-19 23:43:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
+
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=j3QDECzC#iHsy2RqaqBKDfJPW4BMGC6Y5Tv7zIY1usLMYSZUy4SE",
+
+ALIVE IMG: process.env.ALIVE_IMG || "" ,
+
+ALIVE MSG: process.env.ALIVE_MSG || "" ,
+    
 };
