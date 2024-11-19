9ab79b1a8fc00af78e1c3d6311eebb8788c9d708
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-19 18:02:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "27w10SwA#cjp0s8lEhDpOYRV8SjW0R1AnqPDMI0aQVp8LuIl-6cE",
+SESSION_ID: process.env.SESSION_ID || "iLInHDRY#99fIKd1YCqAyhCKkhrEB0uyGN_A7qz7glP7weW604l4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/botlogo.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Code-X-RuWA-V1 I am Alive Now!",
 };
