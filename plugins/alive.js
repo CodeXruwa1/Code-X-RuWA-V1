92eb62c5df09df951ab0f50e32986305e66b9884
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-19 21:24:48 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,9 +10,9 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let aliveImg = `https://github.com/CodeXRuWA/Code-X-RuWA-V1-Alive-img/blob/main/Code-X-RuWA-V1%20Alive%20Logo.jpg?raw=true`
+let aliveImg = 'https://github.com/CodeXRuWA/Code-X-RuWA-V1-Alive-img/blob/main/Code-X-RuWA-V1%20Alive%20Logo.jpg?raw=true'
 
-let aliveMsg = `😈 𝐇𝐞𝐥𝐥𝐨, 𝐈 𝐀𝐦 𝘾𝙤𝙙𝙚-𝙓-𝙍𝙪𝙬𝙖-𝙑1         ━╬٨﹏ـﮩﮩ`ᵐᵈ ʷʰᵃᵗˢᵃᵖᵖ ᵇᵒᵗ´﹏٨ـﮩﮩـ╬━ 
+let aliveMsg = '😈 𝐇𝐞𝐥𝐥𝐨, 𝐈 𝐀𝐦 𝘾𝙤𝙙𝙚-𝙓-𝙍𝙪𝙬𝙖-𝙑1         ━╬٨﹏ـﮩﮩ`ᵐᵈ ʷʰᵃᵗˢᵃᵖᵖ ᵇᵒᵗ´﹏٨ـﮩﮩـ╬━ 
 
 ☣️ 𝐈 𝐀𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰!
 
@@ -24,7 +24,7 @@ let aliveMsg = `😈 𝐇𝐞𝐥𝐥𝐨, 𝐈 𝐀𝐦 𝘾𝙤𝙙𝙚-𝙓-�
 
 💠 Mᴀɪɴ Bᴏᴛ W/A Gʀᴏᴜᴘ - https://chat.whatsapp.com/CViytr0TGpM4gLedmxnFi5
 
-█▃▃▃▃▃▃▃▃        ▃▃▃▃▃▃▃▃█`
+█▃▃▃▃▃▃▃▃        ▃▃▃▃▃▃▃▃█'
     
 return await conn.sendMessage(from,{image: {url: aliveImg },caption: aliveMsg },{quoted: mek})
 }catch(e){
