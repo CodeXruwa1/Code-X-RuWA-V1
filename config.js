8d96428bb0f50d4b4cbebd8521b634e660ebc837
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-19 17:25:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+SESSION_ID: process.env.SESSION_ID || "27w10SwA#cjp0s8lEhDpOYRV8SjW0R1AnqPDMI0aQVp8LuIl-6cE",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/botlogo.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Code-X-RuWA-V1 I am Alive Now!",
 };
