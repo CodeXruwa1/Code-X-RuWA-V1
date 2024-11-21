73f1d3797e9dc542ea4b7ec67300bfd7c06500eb
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-21 12:27:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "3iwXSaQD#k7A0I0JiXDZPlhTQio3bYFhgWxeaL4zcr-xcaZZkryU",
-ALIVE IMG: process.env.ALIVE_IMG || "https://github.com/CodeXRuWA/Code-X-RuWA-V1-Alive-img/blob/main/Code-X-RuWA-V1%20Alive%20Logo.jpg",
+ALIVE IMG: process.env.ALIVE_IMG || "",
 ALIVE MSG: process.env.ALIVE_MSG || "Hello, I am Code-X-RuWA-V1 I am Alive Now!",
 };
