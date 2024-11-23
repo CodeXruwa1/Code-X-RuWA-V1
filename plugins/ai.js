f9c650db380540e95aa591ce39267895c8d45028
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-23 20:41:08 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -16,23 +16,3 @@ console.log(e)
 reply(`${e}`)
 }
 })
-
-
-const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
-
-cmd({
-    pattern: "openai",
-    desc: "openai",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let data = await fetchJson(`https://api.ibeng.tech/api/info/gpt4?q=${q}`)
-reply(`${data.data}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
