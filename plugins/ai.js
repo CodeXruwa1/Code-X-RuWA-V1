86717a4da4764b0f81332f47bc1adcf1b8db9180
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-23 20:36:47 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -36,22 +36,3 @@ console.log(e)
 reply(`${e}`)
 }
 })
-
-
-
-
-
-import fetch from 'node-fetch'
-
-let handler = async(m, { conn, text }) => {
-
-  if (!text) throw `Mau Nanya Apa?`
-    let res = await fetch(`https://api.ibeng.tech/api/info/gpt4?text=${text}&apikey=${global.ibeng}`)
-    let komcol = await res.json()
-   m.reply(komcol.data.data)
-}
-handler.help = ['openai']
-handler.tags = ['stalker']
-handler.command = /^(openai|lia)$/i
-handler.limit = true
-export default handler
