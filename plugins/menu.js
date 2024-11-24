05bbe3785a6242e8956d9efb2bd71e0c842127a6
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-24 22:20:45 +0530

    😈ᴄᴏᴅᴇXʀᴜᴡᴀ ᴠ1😈

--- plugins/menu.js
+++ plugins/menu.js
@@ -3,76 +3,28 @@ const os = require("os")
 const {runtime} = require('../lib/functions')
 
 cmd({
-    pattern: "xmenu",
+    pattern: "bugmenu",
     desc: "menu",
     category: "main",
-    react: "📚",
+    react: "⚠️️",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
 let desc = `
-*😈 Hello* ${pushname}
+*😈welcome dangerous zone⚠️️* ${pushname}
 
-*ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1 MENU📲*
+*𝐂𝐨𝐝𝐞𝐗 BUG MENU📲*
 ┌────────────
-😈 *📚ALL COMMANDS*└────────────
+😈 *📥ALL BUG COMMANDS*└────────────
 ┌────────────
-😈 *📥DOWNLOAD COMMANDS*
+😈 *☠️COMMANDS*
 │
-│ ✑ _.song_
-│ ✑ _.video_
-│ ✑ _.fb_
-│ ✑ _.tt_
-└────────────
-┌────────────
-😈 *🔎SEARCH COMMANDS*
-│
-│ ✑ _.yts_
-│ ✑ _.img_
-└────────────
-┌────────────
-😈 *🎊CONVERTED COMMANDS*
-│
-│ ✑ _.sticker_
-│ ✑ _.tts_
-│ ✑ _.trt_
-└────────────
-┌────────────
-😈 *👸🏻AI COMMANDS*
-│
-│ ✑ _.ai_
-└────────────
-┌────────────
-😈 *🧧MAIN COMMANDS*
-│
-│ ✑ _.owner_
-│ ✑ _.ping_
-│ ✑ _.system_
-└────────────
-┌────────────
-😈 *🎯GROUP COMMANDS*
-│
-│ ✑ _.promote_
-│ ✑ _.demote_
-│ ✑ _.remove_
-│ ✑ _.kick_
-│ ✑ _.setwelcome_
-│ ✑ _.setgoodbye_
-│ ✑ _.getpic_
-└────────────
-┌────────────
-😈 *👤OWNER COMMANDS*
-│
-│ ✑ _.block_
-│ ✑ _.unblock_
-│ ✑ _.jid_
-│ ✑ _.gjid_
-│ ✑ _.restart_
+│ ✑ _.soon_
 └────────────
 
-> *ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1😈*
+> *𝐏𝐨𝐰𝐞𝐫𝐝 𝐁𝐲 𝐂𝐨𝐝𝐞𝐗😈*
 `
 return await conn.sendMessage(from,{image: {url: `https://cdn.ironman.my.id/i/g1mmt2.jpg`},caption: desc},{quoted: mek})
 }catch(e){
