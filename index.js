c48cb44de3fd2d1e2be918a3e01b67d8b3b0812f
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-24 10:22:17 +0530

    😈ᴄᴏᴅᴇXʀᴜᴡᴀ ᴠ1😈

--- index.js
+++ index.js
@@ -174,6 +174,8 @@ mek.type === "stickerMessage"
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
 
+//=============================================================
+        
 })
 }
 app.get("/", (req, res) => {
