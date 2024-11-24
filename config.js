99cacd6b2a6e60fe83e2baca91c218952abb18ea
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-24 15:50:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=6zYT3S4D#nHw4oDTjHYz-z3XzszQoD2GCRjZgxuFiDZciRMq7gzM",
-MONGODB: process.env.MONGODB || "mongodb://mongo:qXQxdAbpntDdvsSpaIgIeyNVCyRdLZid@autorack.proxy.rlwy.net:22892",
+MONGODB: process.env.MONGODB || "mongodb://mongo:yqzUFafwPiAfqersdkRLcGAeDdYzbvuw@junction.proxy.rlwy.net:59749",
 };
 
