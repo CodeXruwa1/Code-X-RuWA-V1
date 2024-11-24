ef913b825856e67e742d39c47d00e9582f20506b
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-24 17:04:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=W74RBKqB#t11g8f8zgzMm7bXBTYDhRIwDVZDEGEjAFHg6WhxhFEM",
+SESSION_ID: process.env.SESSION_ID || "",
 MONGODB: process.env.MONGODB || "mongodb://mongo:yqzUFafwPiAfqersdkRLcGAeDdYzbvuw@junction.proxy.rlwy.net:59749",
 };
