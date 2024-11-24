9ac2a0a92832be7c0dbe51069a933f26341b10d6
Code X Ruwa <eonobdggg12@gmail.com>
2024-11-24 17:03:34 +0530

    😈ᴄᴏᴅᴇXʀᴜᴡᴀ ᴠ1😈

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=6zYT3S4D#nHw4oDTjHYz-z3XzszQoD2GCRjZgxuFiDZciRMq7gzM",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=W74RBKqB#t11g8f8zgzMm7bXBTYDhRIwDVZDEGEjAFHg6WhxhFEM",
 MONGODB: process.env.MONGODB || "mongodb://mongo:yqzUFafwPiAfqersdkRLcGAeDdYzbvuw@junction.proxy.rlwy.net:59749",
 };
